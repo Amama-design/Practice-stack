7e20e318765cbf8a9850e659e3961d02f5468bc4
Amama-design <156662647+Amama-design@users.noreply.github.com>
2024-02-20 16:40:55 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Add your MongoDB connection code here
+
+// Add your API routes here
+
+
+
+
+
+
+
+
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+
+
